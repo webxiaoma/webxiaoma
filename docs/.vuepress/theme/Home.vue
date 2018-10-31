22655bad99654c3fb82bcb02da9704242ae3b995
webxiaoma <webxiaoma@qq.com>
2018-10-31 21:07:35 +0800

    a

--- docs/.vuepress/theme/Home.vue
+++ docs/.vuepress/theme/Home.vue
@@ -68,7 +68,8 @@ export default {
     actionLink () {
       return {
       }
-    }
+    },
+    
   }
 }
 </script>
--- docs/.vuepress/theme/Layout.vue
+++ docs/.vuepress/theme/Layout.vue
@@ -125,7 +125,6 @@ export default {
       ]
     }
   },
-
   mounted () {
     window.addEventListener('scroll', this.onScroll)
 
